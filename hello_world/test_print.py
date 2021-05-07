dece76980f9cef3193c6d2506fcb43f15bfc083e
indrajit <indrajit@gmail.com>
2021-05-07 14:32:17 +0530

    add test print file

---
+++ hello_world/test_print.py
@@ -0,0 +1,13 @@
+"""
+This file is to test if the package can be run
+"""
+
+
+class HelloWorld:
+    def __init__(self):
+        pass
+
+    def print(self) -> None:
+        """Prints a message to the std out
+        """
+        print("Hello World! Good to see you.")
