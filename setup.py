6cb142109f2d7413d06931d41c7fca7363907500
indrajit <indrajit@gmail.com>
2021-05-07 14:47:27 +0530

    first packaging

---
+++ setup.py
@@ -0,0 +1,11 @@
+"""
+This file is to combine all the packages available into a distribution bundle
+"""
+from setuptools import setup
+
+setup(
+    name="bundle_by_indrajit",
+    version="1.0.0",
+    description="Ready to use package distribution bundle by indrajit",
+    packages=["hello_world"],
+)
